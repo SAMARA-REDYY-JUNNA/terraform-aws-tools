4c44a1930f7cef2d767cfe0a723d76ddbfeab0a3
samarareddy <samara.junna@gmail.com>
2025-05-08 09:48:01 +0530

    tools

---
+++ cicd/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "5.92.0"
+        }
+    }
+    backend "s3" {
+      bucket = "mydaws-remote-state1"
+      key  = "expense-cicd-jenkins"
+      region = "us-east-1"
+      dynamodb_table = "mydaws-locking"
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
