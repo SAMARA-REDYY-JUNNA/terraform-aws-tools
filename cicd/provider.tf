dcabda9e5620058ffaac05e9c56fa96f5e5ac6a1
samarareddy <samara.junna@gmail.com>
2025-07-03 09:50:17 +0530

    functions

--- cicd/provider.tf
+++ cicd/provider.tf
@@ -2,11 +2,11 @@ terraform {
     required_providers {
         aws = {
         source  = "hashicorp/aws"
-        version = "5.92.0"
+        version = "5.95.0"
         }
     }
     backend "s3" {
-      bucket = "mydaws-remote-state2"
+      bucket = "mydaws-remote-state12"
       key  = "expense-cicd-jenkins"
       region = "us-east-1"
       dynamodb_table = "mydaws-locking"
