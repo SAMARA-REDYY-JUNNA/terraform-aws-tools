a7253629d31648526c45b997b3ab0ea2fa16d641
samarareddy <samara.junna@gmail.com>
2025-07-03 10:39:36 +0530

    functions

--- cicd/main.tf
+++ cicd/main.tf
@@ -4,7 +4,7 @@ module "jenkins" {
   name = "jenkins-tf"
 
   instance_type          = "t2.large"
-  vpc_security_group_ids = ["sg-058cdcd413276c79f"] #replace your SG
+  vpc_security_group_ids = ["sg-0d9e4f10ad5609d24"] #replace your SG
   subnet_id = "subnet-02534128e32397cb0" #replace your Subnet
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins.sh")
@@ -26,7 +26,7 @@ module "jenkins_agent" {
   name = "jenkins-agent"
 
   instance_type          = "t2.large"
-  vpc_security_group_ids = ["sg-058cdcd413276c79f"]
+  vpc_security_group_ids = ["sg-0d9e4f10ad5609d24"]
   # convert StringList to list and get first element
   subnet_id = "subnet-02534128e32397cb0"
   ami = data.aws_ami.ami_info.id
